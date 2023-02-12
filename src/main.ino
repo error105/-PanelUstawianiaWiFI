81fab50cc1441218d2af5cd770ac4b54bc5633b3
Lukasz <error105@interia.pl>
2023-02-12 10:53:41 +0100

    12.02.2023

--- src/main.ino
+++ src/main.ino
@@ -3,45 +3,60 @@
 #include <ESP8266WebServer.h>
 #include <LittleFS.h>
 
-String ssidbac = "LicznikOdwiedzin";
-String passbac = "1qaz@WSX";
+// Dane APka, pod jakim bedzie sie rozglaszal nasz ESP.
+String ssidbac = "AP-01";
+String passbac = "12345678";
+
+// Zmienne do odczytu danych WiFi z pliku tekstowego
 String ssid, password;
-
+// Nazwa pliku gdzie trzymamy poswiadczenia
 const char* fsFilename = "/wifi.txt";
-
+// Serwer www
 ESP8266WebServer server(80);
+
 
 void setup() {
   Serial.begin(115200);
+  // Ponizszy wpis odczytuje dane WiFi z pliku wifi.txt
   readWifiCredentials();
   Serial.println();
   Serial.println("_________________");
   Serial.println();
+  
+  // Staramy sie nawiazac polaczenie, i tworzymy zmienna lokalna counter - do zliczania ile razy juz sie laczylismy
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   int counter = 0;
   
   while (WiFi.status() != WL_CONNECTED && counter < 40) {
+    // Ustawiamy ile czasu mamy poswiecic na polaczenie z WiFi (250ms * 40)
     delay(250);
     counter++;
     }
     if (WiFi.status() != WL_CONNECTED) {
-      Serial.println("Creating AP...");
+      // Jesli przekroczylismy czas polaczenia, tworzymy AP
+      Serial.println("Tworzenie AP...");
       WiFi.mode(WIFI_AP);
       WiFi.softAP(ssidbac, passbac);
+      // I odpalamy serwer na ktorym wklepiemy nasze nowe dane
       startWebServer();
     } else {
-      Serial.println("Connected to WiFi");
-      Serial.println("IP address: ");
+      // Poalczenie sie powiodlo, wydrukukujmy IP
+      Serial.println("Polaczono z WiFi");
+      Serial.println("Adres IP: ");
       Serial.println(WiFi.localIP());
-      startWebServer();
     }
 }
 
 void loop() {
-  server.handleClient();
+  // Utrzymywanie serwera WWW przy zyciu jesli nie udalosie polaczyc z Siecia WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    server.handleClient();}
+  // Dalej wstawiamy juz nasz kod
+
 }
 
+// Odczytywanie danych z pliku.
 void readWifiCredentials() {
   if (LittleFS.begin()) {
     if (LittleFS.exists(fsFilename)) {
@@ -57,6 +72,7 @@ void readWifiCredentials() {
   }
 }
 
+// Zapisywanie danych w pliku i restart ESP
 void writeWifiCredentials(String ssid, String password) {
   if (LittleFS.begin()) {
     LittleFS.format();
@@ -68,6 +84,7 @@ void writeWifiCredentials(String ssid, String password) {
   }
 }
 
+// Strona WWW na ktorej mozemy wprowadzic nasze nowe poswiadczenia
 void handleRoot() {
   String html = "<html><body><center>";
   html += "<h1>Ustawienia WiFi i API</h1>";
@@ -86,6 +103,7 @@ void handleSet() {
   }
 }
 
+// Wiersz odpowiedzialny za start strony WWW
 void startWebServer() {
   server.on("/", handleRoot);
   server.on("/set", handleSet);
